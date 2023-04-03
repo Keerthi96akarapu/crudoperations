e64dbf010b2f95e6a144463b42daf0194e98285f
keerthi <Keerthi96akarapu>
2023-04-03 15:38:50 +0530

    crud operations

---
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Product;
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+	Product findByName(String name);
+
+	Product save(Product product);
+
+}
